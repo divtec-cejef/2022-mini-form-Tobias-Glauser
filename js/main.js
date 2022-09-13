800a12bb764838f17fefb2df48c1d4cc8c137cec
Tobias Glauser <94681693+Tobias-Glauser@users.noreply.github.com>
2022-09-13 11:01:54 +0200

    mise en place du formulaire

--- js/main.js
+++ js/main.js
@@ -6,3 +6,19 @@
 'use strict';
 
 console.log('Test liaison JS');
+
+const formulaire = document.querySelector('form');
+const listBackground = document.getElementById('background');
+
+// A l'envoie du formulaire
+formulaire.addEventListener('submit', function (event) {
+    event.preventDefault(); // annule le comportement par défaut
+
+    alert("Envoyé !");
+});
+
+// Au changement de la valeur de la liste
+listBackground.addEventListener('change', function () {
+    const image = `./img/backgrounds/${listBackground.value}`
+    document.body.style.backgroundImage = `url(${image})`
+});
