0832dc0b09ed5cd74b01cf71d751074fae258d13
Tobias Glauser <94681693+Tobias-Glauser@users.noreply.github.com>
2022-09-20 08:50:20 +0200

    fin de l'exercice

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 /**
- * @author Steve Fallet
+ * @author Tobias Glauser
  * @since 2022.09.06
  */
 
@@ -10,11 +10,21 @@ console.log('Test liaison JS');
 const formulaire = document.querySelector('form');
 const listBackground = document.getElementById('background');
 
+// animation du bouton
+const btCreer = document.querySelector('[type="submit"]')
+btCreer.addEventListener('animationend', function (){
+    alert("Envoyé !");
+    formulaire.reset();
+    btCreer.classList.remove('animate__heartBeat')
+});
+
+
 // A l'envoie du formulaire
 formulaire.addEventListener('submit', function (event) {
     event.preventDefault(); // annule le comportement par défaut
 
-    alert("Envoyé !");
+    // Animation du bouton
+    btCreer.classList.add('animate__heartBeat');
 });
 
 // Au changement de la valeur de la liste
